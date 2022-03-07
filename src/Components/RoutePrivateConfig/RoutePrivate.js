9637bd34ac4a62e1d0869361e420ddb8e3b8930a
Carlos Sierra <carlossierra850@gmail.com>
2022-03-06 22:38:24 -0400

    inserting socketio

--- src/Components/RoutePrivateConfig/RoutePrivate.js
+++ src/Components/RoutePrivateConfig/RoutePrivate.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import {Route, Redirect} from "react-router";
 import Loading from '../Utils/Loading'
 import { getCompanyAction } from '../../Actions/ActionsAuth';
+import io from "socket.io-client";
 const PrivateRoute = ({component: Component, ...props}) => {
     let dispatch = useDispatch();
     const [mounted, setMounted] = useState(false)
@@ -12,7 +13,12 @@ const PrivateRoute = ({component: Component, ...props}) => {
            await dispatch(getCompanyAction(token))
            setMounted(true)
         }
-        authCompany();     
+        authCompany();    
+        const socket = io("http://localhost:4000");
+        socket.emit('notifications', {
+            message: 'Hello',
+            username: 'World'
+          });
     }, [dispatch])
     const {auth} = useSelector(state => state.auth)
     if (!mounted) return <Loading />
