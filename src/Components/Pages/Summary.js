550217637d9474b7f9ca4e5d6c6a6c219c63351d
Carlos Sierra <carlossierra850@gmail.com>
2022-04-03 23:08:32 -0400

    add: some style for summary

--- src/Components/Pages/Summary.js
+++ src/Components/Pages/Summary.js
@@ -1,9 +1,13 @@
 import { Card, Grid, Stack, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React,{useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getSummaryAction } from '../../Actions/ActionsRequests'
+import { formatAmount } from '../../helpers';
 import Side from '../Layout/Side';
 import Loading from '../Utils/Loading';
+import StoreIcon from '@mui/icons-material/Store';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 const Summary = () => {
     const dispatch = useDispatch()
     const loadSummary = () => dispatch(getSummaryAction())
@@ -15,17 +19,43 @@ const Summary = () => {
         <Side>
           {summary ? (
             <>
+             <Stack paddingBottom={2}>
              <h3>Summary</h3>
-            <Stack container direction='column' spacing={3}>
+             <Typography>Eventual data of the last 30 days</Typography>
+             </Stack>
+            <Grid
+          gap={3}
+          container
+          direction="row"
+          justifyContent="start"
+          alignItems="center"
+        >
             <Card sx={{padding : '1rem'}}>
-              <Typography>Ingresos de los últimos 30 días</Typography>
-              {summary.earnings} 
+              <Grid container direction='row' justifyContent="space-between">
+                <Typography>Income</Typography>
+              <StoreIcon/>
+              </Grid>
+              <Box className="format-amount-summary" color={'primary.main'}>
+                {formatAmount(summary.earnings)} 
+              </Box>
              </Card>
              <Card sx={{padding : '1rem'}}>
-              <Typography>Numero Total de Ventas</Typography>
-              {summary.earnings} 
+              <Typography>Number of sales</Typography>
+              <Box className="format-amount-summary" color={'primary.main'}>
+                {summary.orderLength}
+                <ShoppingCartIcon/>
+              </Box>
              </Card>
-            </Stack>
+             <Card sx={{padding : '1rem'}}>
+             <Grid container direction='row' justifyContent="space-between">
+               <Typography>Monthly fee</Typography>
+              <Payment/>
+              </Grid>
+              <Box className="format-amount-summary" color={'primary.main'}>
+                {formatAmount((summary.earnings * 0.02).toFixed(2), '$', '(-2%)')}
+              </Box>
+             </Card>
+            </Grid>
             </>
           ) :
             (<Loading/>)
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,8 @@
-export const formatAmount = (amount, currency) => {
+export const formatAmount = (amount, currency = '$', txt) => {
    let toNumber = Number(amount)
    if(amount <= 0) return 'Free'
-   return `${currency} ${toNumber.toFixed(2)}`;
+   if(txt)return (<p className="format-amount"><span>{txt} </span><span>{currency}</span>{toNumber.toFixed(2)}</p>);
+   return (<p className="format-amount"><span>{currency}</span>{toNumber.toFixed(2)}</p>);
 }
 export const formatAmountToNumber = (str) => {
    let number = Number(str.substring(1))
